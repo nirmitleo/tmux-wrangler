0b91c0240357d9db929cb30cafdd03c4e74aef98
Nirmit Dalal <nirmit@invideo.io>
2024-07-14 17:19:34 +0530

    inital setup

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "tmux wrangler";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        pythonEnv = pkgs.python312.withPackages (ps: with ps; [
+          pip
+        ]);
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [ 
+            pythonEnv
+          ];
+          shellHook = ''
+            export LANG=C.UTF-8
+            export POETRY_VIRTUALENVS_IN_PROJECT=true
+            export PYTHONDONTWRITEBYTECODE=1
+
+            # Create venv if it doesn't exist
+            if [ ! -d "venv" ]; then
+              python3 -m venv venv
+              venv/bin/pip install -U pip setuptools
+              venv/bin/pip install poetry==1.8.3
+            fi
+            
+            # Activate venv
+            source ./venv/bin/activate
+            echo "✅ Activated venv"
+
+             # Verify Poetry installation
+            poetry --version
+          '';
+        };
+      }
+    );
+}
